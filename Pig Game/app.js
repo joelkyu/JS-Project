bab7730c900f2b94417d040ef377cbb47b4457cf
Joe Yu <joe.yu@mappedin.ca>
2019-08-14 22:56:53 -0400

    added setting winning score feature

--- Pig Game/app.js
+++ Pig Game/app.js
@@ -1,4 +1,4 @@
-var scores, roundScore, lastRoll, activePlayer, gamePlaying;
+var scores, roundScore, lastRoll, activePlayer, gamePlaying, winningScore;
 
 // Initialize
 init();
@@ -18,8 +18,7 @@ function switchPlayer() {
 function diceRoll() {
     if (gamePlaying) {
         // dice roll and change dice image
-        //var dice = Math.floor(Math.random() * 6) + 1;
-        var dice = 6;
+        var dice = Math.floor(Math.random() * 6) + 1;
         var diceDOM = document.querySelector('.dice');
         diceDOM.style.display = 'block';
         diceDOM.src = 'dice-' + dice + '.png';
@@ -43,10 +42,22 @@ function diceRoll() {
 
 function holdBtn() {
     if (gamePlaying) {
+        //checks the winning score
+        var winningScore = document.querySelector('.winning-score').value
+
+        // use coercion to check if value is valid
+        if (!winningScore || winningScore < 1) {
+            winningScore = 10;
+        }
+
+        // adds current score to total score
         scores[activePlayer] += roundScore
         document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
 
-        if (scores[activePlayer] >= 100) {
+        
+        
+        // shows the player winning 
+        if (scores[activePlayer] >= winningScore) {
             document.querySelector('#name-' + activePlayer).textContent = 'Gagner!';
             document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
             document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
@@ -59,25 +70,32 @@ function holdBtn() {
 }
 
 function init() {
+    // resets Player 1 Display
     document.getElementById('current-0').textContent = 0;
     document.getElementById('score-0').textContent = 0;
     document.querySelector('.player-0-panel').classList.add('active');
     document.querySelector('.player-0-panel').classList.remove('winner');
     document.querySelector('#name-0').textContent = 'Player 1';
 
+    // resets Player 2 Display
     document.getElementById('current-1').textContent = 0;
     document.getElementById('score-1').textContent = 0;
     document.querySelector('.player-1-panel').classList.remove('active'),
-        document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
     document.querySelector('#name-1').textContent = 'Player 2';
 
+    // resets internal score and variables
     scores = [0, 0];
     roundScore = 0;
     lastRoll = 0;
     activePlayer = 0;
+    winningScore = '';
+    document.querySelector('.winning-score').placeholder = 'Set Winning Score'
 
+    // hides the dice
     document.querySelector('.dice').style.display = 'none';
 
+    // resets the state of the game
     gamePlaying = true;
 }
 
